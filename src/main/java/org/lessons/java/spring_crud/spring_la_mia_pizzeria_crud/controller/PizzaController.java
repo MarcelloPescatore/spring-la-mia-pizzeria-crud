33bcc0ae19c26589e35b3486ebbf383fc2bfe4aa
MarcelloPescatore <pescatoremarcello@gmail.com>
2025-03-19 19:40:50 +0100

    step 2 + step 3

--- src/main/java/org/lessons/java/spring_crud/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_crud/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -18,9 +21,24 @@ public class PizzaController {
     @Autowired
     private PizzaRepository repository;
     
+    @GetMapping("/{id}")
+    public String getPizzaDetails(@PathVariable("id") Integer id, Model model) {
+        Pizza pizza = repository.findById(id).get();
+    
+        model.addAttribute("pizza", pizza);
+        return "pizze/show";
+    }
+
     @GetMapping
-    public String index(Model model) {
-        List<Pizza> pizze = repository.findAll();
+    public String getMethodName(@RequestParam(name ="nome", required = false) String nome, Model model) {
+        List<Pizza> pizze;
+        
+        if (nome != null && !nome.isEmpty()) {
+            pizze = repository.findByNomeContaining(nome); 
+        } else {
+            pizze = repository.findAll(); 
+        }
+
         model.addAttribute("pizze", pizze);
         return "pizze/index";
     }
